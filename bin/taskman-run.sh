4b6a76cb7459152fb0c82cf5db0db0d3ebbbd1b2
koe <koe@localhost>
2016-06-23 01:40:21 +0300

    pause after running workers (before running master) in taskman-run.sh

--- bin/taskman-run.sh
+++ bin/taskman-run.sh
@@ -26,7 +26,22 @@ ws=$(bin/taskman-worker.sh "$task" status)
 
 if [[ "$ms" == "no master is running" && "$ws" == "workers: 0" ]]; then
 
+	# It is best to keep the order: workers first, master next. Otherwise workers
+	# won't quit in case there are no tasks.
+
 	bin/taskman-worker.sh "$task" limit $N
+
+	# Sometimes master cant start but workers keep running so it prevents the master
+	# from running on next invokation at all. The problem appeared somewhere in Apr to May 2016.
+	# Probably it is related to the "start a bunch of workers" +
+	# "immediately start the master with 5 secs timeout".
+	# Increasing master start timeout is not reliable (it has no access to the number
+	# of workers that has just been run).
+	# Let's try introduce N/2 secs pause here.
+	((pause=N/2))
+	echo waiting $pause secs for workers to get ready
+	sleep $pause
+
 	bin/taskman-master.sh "$task" start
 
 else
